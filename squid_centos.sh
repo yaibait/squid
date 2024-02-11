f5f00e23e6e842e500c83627b09d465f233f4c37
yaibait <binhnguyen7968@gmail.com>
2024-02-11 10:41:21 +0700

    Update squid_centos.sh

--- squid_centos.sh
+++ squid_centos.sh
@@ -17,8 +17,7 @@ htpasswd -b -c /etc/squid/passwd $squid_user $squid_password
 
 mv /etc/squid/squid.conf /etc/squid/squid.conf.bak
 touch /etc/squid/blacklist.acl
-wget -O /etc/squid/squid.conf  https://raw.githubusercontent.com/khaledalhashem/squid/master/squid_centos.conf
-
+wget -O /etc/squid/squid.conf  https://raw.githubusercontent.com/yaibait/squid/master/squid_centos.conf
 iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
 #/sbin/iptables-save
 /sbin/service iptables save
