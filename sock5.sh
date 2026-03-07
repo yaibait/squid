a0e5af9276fc80bffd19a57099527fd58ee25ec1
yaibait <binhnguyen7968@gmail.com>
2026-03-07 11:10:09 +0700

    Refactor sock5.sh for dnf and configurable port
    Updated the script to use dnf instead of yum for package installation and made the port configurable. Added firewall commands for port management.

--- sock5.sh
+++ sock5.sh
@@ -2,43 +2,42 @@
 
 socks_user=binh
 socks_password=Quacam123
+PORT=4977
 
-yum -y install epel-release
-yum -y install dante-server
+dnf install -y epel-release
+dnf install -y dante-server
 
 useradd -M -s /sbin/nologin $socks_user
 echo "$socks_user:$socks_password" | chpasswd
-
-mv /etc/sockd.conf /etc/sockd.conf.bak 2>/dev/null
 
 IFACE=$(ip route get 1 | awk '{print $5;exit}')
 
 cat > /etc/sockd.conf <<EOF
 logoutput: syslog
 
-internal: 0.0.0.0 port = 4977
+internal: 0.0.0.0 port = $PORT
 external: $IFACE
 
 method: username
+
 user.privileged: root
 user.notprivileged: nobody
 
 client pass {
     from: 0.0.0.0/0 to: 0.0.0.0/0
-    log: connect disconnect
 }
 
 socks pass {
     from: 0.0.0.0/0 to: 0.0.0.0/0
     command: connect
     method: username
-    log: connect disconnect
 }
 EOF
 
-iptables -I INPUT -p tcp --dport 4977 -j ACCEPT
-/sbin/service iptables save
+# mở firewall
+firewall-cmd --permanent --add-port=$PORT/tcp
+firewall-cmd --reload
 
-systemctl restart sockd
 systemctl enable sockd
+systemctl restart sockd
 systemctl status sockd
